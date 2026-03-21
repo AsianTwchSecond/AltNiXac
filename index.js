bb6e722a1304b8ce2be8258306f469998ef07583
AsianTwchSecond <kurtallenbros111@gmail.com>
2026-03-21 08:41:00 +0800

    Update bot connection details and login command

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ function joinBot() {
   addLog("Starting bot...")
 
   bot = mineflayer.createBot({
-    host: "play.bingungsmp.top",
-    username: "AltNiXac",
+    host: "leztusasmp.xyz",
+    username: "Xacrifizee_",
     version: false
   })
 
@@ -35,7 +35,7 @@ function joinBot() {
     connecting = false
     addLog("Bot spawned")
 
-    setTimeout(() => bot.chat("/login kurtalle"), 3000)
+    setTimeout(() => bot.chat("/login <kurt>"), 3000)
     setTimeout(() => bot.chat("/server ecocpvp"), 6000)
 
     bot.jumpInterval = setInterval(() => {
